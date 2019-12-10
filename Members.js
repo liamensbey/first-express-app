e45658952d22d3d43ac1808239446b3bd55eece1
Liam Ensbey <liam.ensbey@gmail.com>
2019-12-10 18:01:33 +1300

    Changed app.js

---
+++ Members.js
@@ -0,0 +1,16 @@
+const members = [
+    {
+        id: 1,
+        name: 'Liam Ensbey',
+        email: 'liam.ensbey@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'John Doe',
+        email: 'john.doe@gmail.com',
+        status: 'inactive'
+    }
+]
+
+module.exports = members;
